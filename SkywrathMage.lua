b7389f0043e9362e7963024f632584e2f4a09133
YierciiCA <Jerffelly@gmail.com>
2017-02-02 20:49:42 +0200

    Skywrath Mage ALPHA 0.0.0.3

--- SkywrathMage.lua
+++ SkywrathMage.lua
@@ -1,79 +1,52 @@
 local SkyWRTH = {}
 
-SkyWRTH.optionEnable = Menu.AddOption({ "Hero Specific","Skywrath Mage"}, "Enabled", "")
-SkyWRTH.optionKey = Menu.AddKeyOption({ "Hero Specific","Skywrath Mage"}, "Combo Key", Enum.ButtonCode.KEY_SPACE)
-
+SkyWRTH.optionEnable = Menu.AddOption({ "Hero Specific","Skywrath Mage" }, "Enabled", "")
+SkyWRTH.optionKey = Menu.AddKeyOption({ "Hero Specific","Skywrath Mage" }, "Combo Key", Enum.ButtonCode.KEY_SPACE)
 
 function SkyWRTH.OnUpdate()
     if not Menu.IsEnabled(SkyWRTH.optionEnable) then return true end
-    AbilityCombo()
+
+    SkyWRTH.AbilityCombo()
 end
 
-function AbilityCombo()
+function SkyWRTH.AbilityCombo()
+    if not Menu.IsKeyDown(SkyWRTH.optionKey) then return end
 
-if not Menu.IsKeyDown(SkyWRTH.optionKey) then return true end
+    local myHero = Heroes.GetLocal()
 
-local myHero = Heroes.GetLocal()
-if NPC.GetUnitName(myHero) ~= "npc_dota_hero_skywrath_mage" then return true end
+    if NPC.GetUnitName(myHero) ~= "npc_dota_hero_skywrath_mage" then return end
 
-local arcBolt = NPC.GetAbilityByIndex(myHero, 0)
-local conShot = NPC.GetAbilityByIndex(myHero, 1)
-local ancSeal = NPC.GetAbilityByIndex(myHero, 2)
-local mFlare = NPC.GetAbilityByIndex(myHero, 3)
+    local hero = Input.GetNearestHeroToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_ENEMY)
 
-local veil = NPC.GetItem( myHero, "item_veil_of_discord", true)
-local hex = NPC.GetItem( myHero, "item_sheepstick", true)
-local bloth = NPC.GetItem( myHero, "item_bloodthorn", true)
-local eblade = NPC.GetItem( myHero, "item_ethereal_blade", true)
-local orchid = NPC.GetItem( myHero, "item_orchid", true)
-local refresh = NPC.GetItem( myHero, "item_refresher", true)
+    if not hero then return end
 
+    local heroPos = NPC.GetAbsOrigin(hero)
 
+    local arcBolt = NPC.GetAbilityByIndex(myHero, 0)
+    local conShot = NPC.GetAbilityByIndex(myHero, 1)
+    local ancSeal = NPC.GetAbilityByIndex(myHero, 2)
+    local mFlare = NPC.GetAbilityByIndex(myHero, 3)
 
-local mousePos = Input.GetWorldCursorPos()
-local myMana = NPC.GetMana(myHero)
+    local veil = NPC.GetItem(myHero, "item_veil_of_discord", true)
+    local hex = NPC.GetItem(myHero, "item_sheepstick", true)
+    local bloth = NPC.GetItem(myHero, "item_bloodthorn", true)
+    local eblade = NPC.GetItem(myHero, "item_ethereal_blade", true)
+    local orchid = NPC.GetItem(myHero, "item_orchid", true)
+    local refresh = NPC.GetItem(myHero, "item_refresher", true)
 
-local hero = Input.GetNearestHeroToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_ENEMY)
-if veil ~= nil and Ability.IsCastable(veil, myMana) then 
-    Ability.CastPosition(veil, mousePos)
-else
-if hex ~= nil and Ability.IsCastable(hex, myMana) then 
-    Ability.CastTarget(hex, hero)
-    else
-if bloth ~= nil and Ability.IsCastable(bloth, myMana) then 
-    Ability.CastTarget(bloth, hero)
-    else
-if eblade ~= nil and Ability.IsCastable(eblade, myMana) then 
-    Ability.CastTarget(eblade, hero)
-    else
-if orchid ~= nil and Ability.IsCastable(orchid, myMana) then 
-    Ability.CastTarget(orchid, hero)
-    else
-if Ability.IsReady(ancSeal) and NPC.GetMana(myHero) > Ability.GetManaCost(ancSeal) and NPC.IsEntityInRange(hero, myHero, Ability.GetCastRange(ancSeal) - 10) then
-					Ability.CastTarget(ancSeal, hero)
-    else		
-if Ability.IsReady(arcBolt) and NPC.GetMana(myHero) > Ability.GetManaCost(arcBolt) and NPC.IsEntityInRange(hero, myHero, Ability.GetCastRange(arcBolt) - 10) then
-					Ability.CastTarget(arcBolt, hero)
-    else
-if Ability.IsReady(conShot) and NPC.GetMana(myHero) > Ability.GetManaCost(conShot) and NPC.IsEntityInRange(hero, myHero, Ability.GetCastRange(conShot) - 10) then
-					Ability.CastNoTarget(conShot)
-    else                  
-if Ability.IsReady(mFlare) and NPC.GetMana(myHero) > Ability.GetManaCost(mFlare) and NPC.IsEntityInRange(hero, myHero, Ability.GetCastRange(mFlare) - 10) then
-                    Ability.CastPosition(mFlare, mousePos)
-   else
-if refresh ~= nil and Ability.IsCastable(refresh, myMana) then 
-    Ability.CastNoTarget(refresh)
+    local mousePos = Input.GetWorldCursorPos()
+    local myMana = NPC.GetMana(myHero)
 
+    if veil and Ability.IsCastable(veil, myMana) then Ability.CastPosition(veil, heroPos); return end
+    if hex and Ability.IsCastable(hex, myMana) then Ability.CastTarget(hex, hero); return end
+    if bloth and Ability.IsCastable(bloth, myMana) then Ability.CastTarget(bloth, hero); return end
+    if eblade and Ability.IsCastable(eblade, myMana) then Ability.CastTarget(eblade, hero); return end
+    if orchid and Ability.IsCastable(orchid, myMana) then Ability.CastTarget(orchid, hero); return end
+    if Ability.IsCastable(ancSeal, myMana) and NPC.IsEntityInRange(hero, myHero, Ability.GetCastRange(ancSeal) - 10) then Ability.CastTarget(ancSeal, hero); return end
+    if Ability.IsCastable(arcBolt, myMana) and NPC.IsEntityInRange(hero, myHero, Ability.GetCastRange(arcBolt) - 10) then Ability.CastTarget(arcBolt, hero); return end
+    if Ability.IsCastable(conShot, myMana) and NPC.IsEntityInRange(hero, myHero, Ability.GetCastRange(conShot) - 10) then Ability.CastNoTarget(conShot); return end
+    if Ability.IsCastable(mFlare, myMana) and NPC.IsEntityInRange(hero, myHero, Ability.GetCastRange(mFlare) - 10) then Ability.CastPosition(mFlare, heroPos); return end
+    if refresh and Ability.IsCastable(refresh, myMana) then Ability.CastNoTarget(refresh); return end
+end
 
-end
-end
-end
-end
-end
-end
-end
-end
-end
-end
-end
 return SkyWRTH
